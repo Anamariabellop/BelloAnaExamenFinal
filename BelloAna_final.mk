21465502de81e27757a5c3a536e2def8603aeaad
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-23 16:59:55 +0000

    make

---
+++ BelloAna_final.mk
@@ -0,0 +1,8 @@
+filtro.pdf Canal.png: BelloAnaM_Fourier.py BelloAnaM_canal_ionico.py 
+	python BelloAnaM_Fourier.py
+	python BelloAnaM_canal_ionico.py
+
+#filtro.pdf: BelloAnaM_Fourier.py
+#	python BelloAnaM_Fourier.py
+#BelloAnaM_Fourier.py: Canal_ionico.txt
+#	
